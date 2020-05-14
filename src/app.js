7eaad50ad4884f933202c7d9e346b9c2fc07912f
Eduardo <ebfjunior@gmail.com>
2020-05-14 16:36:40 -0300

    separa configuração de aplicação e configuração de servidor

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express();
-const port = 3000;
 
 app.use(bodyParser.json());
 
@@ -10,7 +9,4 @@ app.get('/', (req, res) => {
   res.send('home sweet home');
 });
 
-
-app.listen(port, () => {
-  console.log('server');
-});
+export default app;
